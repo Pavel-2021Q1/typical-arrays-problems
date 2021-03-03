25ac34784e84bcf906406fdeb619ac00414f11b0
Pavel-2021Q1 <pavel.krol@list.ru>
2021-03-03 17:59:57 +0200

    add task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    if (array != undefined && array.length != 0) {
+        return array.reduce((prev, next) => Math.min(prev, next));
+    }
+    return 0;
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array != undefined && array.length != 0) {
+        return array.reduce((prev, next) => Math.max(prev, next));
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array != undefined && array.length != 0) {
+        return array.reduce((prev, next) => prev + next) / array.length;
+    }
+    return 0;
 }
